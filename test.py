95a414247992697fe8c44f214cf19ad415bef3a2
ShivinDass <shivindass@gmail.com>
2025-04-26 22:51:56 -0500

    b

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from droid.controllers.oculus_controller import VRPolicy
 from droid.robot_env import RobotEnv
+from droid.trajectory_utils.misc import collect_trajectory
 from droid.user_interface.data_collector import DataCollecter
 
 env = RobotEnv()
 controller = VRPolicy(right_controller=True)
-data_collector = DataCollecter(env=env, controller=controller, save_data=False)
+collect_trajectory(env=env, controller=controller, practice=True, reset_robot=True, wait_for_controller=True)
+
+# data_collector = DataCollecter(env=env, controller=controller, save_data=False)
 # user_interface = RobotGUI(robot=data_collector, right_controller=True)
 
-data_collector.collect_trajectory(practice=True)
+# data_collector.collect_trajectory(practice=True)
