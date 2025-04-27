6c971758451eb9167c12b6ec1a88a2f26b07bdb4
ShivinDass <shivindass@gmail.com>
2025-04-27 17:01:18 -0500

    b

--- rollout_policy.py
+++ rollout_policy.py
@@ -139,7 +139,7 @@ def collect_trajectory(
         if (policy is not None) and (not controller_action_info["movement_enabled"]):
             # action = policy.forward(obs)
             action = np.zeros_like(action)
-            action[0] = 0.01
+            action[0] = 0.1
             print("final action", action)
             recording1.append(policy.processed_obs["image_primary"][..., :3])
             recording2.append(policy.processed_obs["image_wrist"][..., :3])
