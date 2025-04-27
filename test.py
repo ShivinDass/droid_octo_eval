c7d7f340156fe76b5aaba7c24c0eaf167e675c26
ShivinDass <shivindass@gmail.com>
2025-04-26 22:52:54 -0500

    b

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from droid.user_interface.data_collector import DataCollecter
 
 env = RobotEnv()
 controller = VRPolicy(right_controller=True)
-collect_trajectory(env=env, controller=controller, practice=True, reset_robot=True, wait_for_controller=True)
+collect_trajectory(env=env, controller=controller, reset_robot=True, wait_for_controller=True)
 
 # data_collector = DataCollecter(env=env, controller=controller, save_data=False)
 # user_interface = RobotGUI(robot=data_collector, right_controller=True)
